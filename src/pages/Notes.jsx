b0035ba797c346aa5f3c2e7537bad963801888d0
Thabang391 <thabangmoshka@gmail.com>
2024-05-23 20:52:05 +0200

    Adding Themes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,76 +1,43 @@
 import { useEffect, useState } from "react";
-import { Query } from "appwrite";
-import { Navigate } from "react-router-dom";
 import db from "../appwrite/databases";
 import NoteForm from "../components/NoteForm";
+import { Query } from "appwrite";
 import Note from "../components/Note";
-// import LogoutButton from "../components/LogoutButton";
-// import { useAuth } from "../context/AuthContext";
-// import ThemeOption from "../components/ThemeOption";
-import LockIcon from "../assets/LockIcon";
+import ThemeOption from "../components/ThemeOption";
 
-const Notes = () => {
+function Notes() {
     const [notes, setNotes] = useState([]);
-    // const [pro, setPro] = useState(false);
-    // const { user } = useAuth();
 
     useEffect(() => {
         init();
     }, []);
 
     const init = async () => {
-        // setPro(user.labels.includes("premium"));
         const response = await db.notes.list([Query.orderDesc("$createdAt")]);
         setNotes(response.documents);
     };
 
     return (
         <>
-            {/* {pro ? (
-                <div id="pro-header">
-                    <div className="tag">
-                        <span>Pro member</span>
-                    </div>
-                    <div className="theme-options">
-                        <ThemeOption
-                            bg="#954ebe"
-                            border="#954ebe"
-                            theme="purple"
-                        />
-                        <ThemeOption bg="#fff" border="black" theme="light" />
-                        <ThemeOption bg="#000" border="#fff" theme="dark" />
-                    </div>
-                </div>
-            ) : (
-                <div id="subscribe-prompt">
-                    <LockIcon />
-                    <p>Unlock purple mode for $1</p>
-                </div>
-            )} */}
-
-            <div id="pro-header">
-                <div className="tag">
-                    <span>Pro member</span>
-                </div>
-                <div className="theme-options">
-                    {/* <ThemeOption bg="#954ebe" border="#954ebe" theme="purple" />
-                    <ThemeOption bg="#fff" border="black" theme="light" />
-                    <ThemeOption bg="#000" border="#fff" theme="dark" /> */}
-                </div>
-            </div>
             <div>
                 <h1>✍️ My Todo List</h1>
             </div>
 
+            <div className="theme-options">
+                <ThemeOption theme="dark" />
+                <ThemeOption theme="light" />
+                <ThemeOption theme="purple" />
+            </div>
+
             <NoteForm setNotes={setNotes} />
 
-            {notes.map((note) => (
-                <Note key={note.$id} noteData={note} setNotes={setNotes} />
-            ))}
-
-            {/* <LogoutButton /> */}
+            <div>
+                {notes.map((note) => (
+                    <Note key={note.$id} setNotes={setNotes} noteData={note} />
+                ))}
+            </div>
         </>
     );
-};
+}
 
 export default Notes;
